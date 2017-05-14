9dbe3f2e41d0b2365f2c8eebc186ad676490d6a5
Mateusz Loskot <mateusz@loskot.net>
2017-05-14 23:45:25 +0200

    Tweak printf

--- fuzz_me.cpp
+++ fuzz_me.cpp
@@ -5,12 +5,19 @@
 
 bool FuzzMe(unsigned char *data, size_t dataSize)
 {
+    if (dataSize > 0)
+    {
     printf("%s: dataSize=%u, data=%.50s\n", __func__, dataSize, data);
     return dataSize > 0 &&
         data[0] == 'F' &&
         data[1] == 'U' &&
         data[2] == 'Z' &&
         data[3] == 'Z';  // :‑<
+    }
+    else
+    {
+        printf("%s: no data\n", __func__);
+    }
 }
 
 extern "C"
